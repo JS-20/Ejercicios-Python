3aacb41fb2d10b710ee33fae2c7451d10625446b
JS-20 <justynesan@gmail.com>
2026-04-26 19:21:07 -0300

    ultima práctica

--- ejercicio_10.py
+++ ejercicio_10.py
@@ -15,5 +15,23 @@
 #                print(f"{letra} es una consonante")
 
 
+# escribir un programa que guarde en variables el monto del ingreso de cada uno de los
+# 6 meses del año, luego calcular y guardar en otra variable el promedio de esos 
+# valores. Por ultimo, mostrar una leyenda que diga "el ingreso promedio en el semestre es de
+# $X" donde X es el valor del promedio calculado.
 
-                
+print("ingrese el monto del ingreso del mes 1")
+mes1 = float(input())
+print("ingrese el monto del ingreso del mes 2")
+mes2 = float(input())
+print("ingrese el monto del ingreso del mes 3")
+mes3 = float(input())
+print("ingrese el monto del ingreso del mes 4")
+mes4 = float(input())
+print("ingrese el monto del ingreso del mes 5")     
+mes5 = float(input())
+print("ingrese el monto del ingreso del mes 6")
+mes6 = float(input())       
+promedio = (mes1 + mes2 + mes3 + mes4 + mes5 + mes6) / 6
+print(f"el ingreso promedio en el semestre es de ${promedio}")
+
